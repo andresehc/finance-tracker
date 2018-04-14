e4524388e80de5ac0844a9b1465b626458f0fc80
Andres <andresehenriquezc@gmail.com>
2018-04-13 22:33:29 -0400

    Add user and stock association plus views

--- app/controllers/user_stocks_controller.rb
+++ app/controllers/user_stocks_controller.rb
@@ -9,4 +9,12 @@ class UserStocksController < ApplicationController
 		flash[:success] = "Stock #{@user_stock.stock.name} vas successfully added to your portfolio!"
 		redirect_to my_portfolio_path
 	end
+
+	def destroy
+		stock = Stock.find(params[:id])
+		@user_stock = UserStock.where(user_id: current_user.id, stock_id: stock.id).first
+		@user_stock.destroy
+		flash[:danger] = "Stock was successfully deleted from portfolio"
+		redirect_to my_portfolio_path
+	end
 end
