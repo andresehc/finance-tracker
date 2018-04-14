aa68634e1b3e146efc50fe9cd2fa4cb28f0fb665
Andres <andresehenriquezc@gmail.com>
2018-04-13 21:00:02 -0400

    Errores en la vista arreglados

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,19 +1,14 @@
 class StocksController < ApplicationController
 
   def search
-  	if params[:stock].present?
+  	if params[:stock].blank?
+		  flash.now[:danger] = "You have to enter a ticker"
+		else
 	    @stock = Stock.new_from_lookup(params[:stock])
-	    if @stock
-	    	respond_to do |format|
-      		format.js {render partial: 'users/result'}
-    		end	    	
-	    else
-	    	flash[:danger] = "You have entered an invalid stock ticker"
-	    	redirect_to my_portfolio_path
-	    end
-	  else
-	  	flash[:danger] = "You have to enter a ticker"
-	  	redirect_to my_portfolio_path
+		  flash.now[:danger] = "You have entered an invalid stock ticker" unless @stock
+	  end
+	  respond_to do |format|
+			format.js {render partial: 'users/result'}
 	  end
   end
 end
--- app/controllers/user_stocks_controller.rb
+++ app/controllers/user_stocks_controller.rb
@@ -4,9 +4,6 @@ class UserStocksController < ApplicationController
 		if stock.blank?
 			stock= Stock.new_from_lookup(params[:stock_ticker])
 			stock.save
-		# else
-		# 	flash.now[:danger]="Stock already in portfolio"
-		# 	render 'users/my_portfolio'
 		end
 		@user_stock = UserStock.create(user: current_user, stock:stock)
 		flash[:success] = "Stock #{@user_stock.stock.name} vas successfully added to your portfolio!"
